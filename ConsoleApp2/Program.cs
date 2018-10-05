7ae412a00360331355d08066098483b05c5702e2
pengmingzhu <1326973099@qq.com>
2018-10-05 14:33:20 +0800

    tijiao

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp2
         {
             Console.WriteLine("nice");
             Console.WriteLine("ok2");
+            string str = null;
             Console.WriteLine();
         }
     }
